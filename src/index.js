2cbc5fb1ad4a35a26390d2bc4943a276698a741c
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-10-27 17:30:47 +0100

    'wip'

--- src/index.js
+++ src/index.js
@@ -3,4 +3,35 @@ import ReactDOM from 'react-dom';
 
 import events from './data/events.json';
 
-ReactDOM.render(<p>Your App goes here....</p>, document.getElementById('root'));
+const element = (
+  <div>
+    					<table className="table">
+							  <thead>
+							    <tr>
+							      <th scope="col">#</th>
+							      <th scope="col">Name</th>
+							      <th scope="col">Place</th>
+							      <th scope="col">Date</th>
+							      <th scope="col">Time</th>
+							    </tr>
+							  </thead>
+							  <tbody>
+				{events.map((arrayElement) => {
+      			return <tr>
+							      <td key={arrayElement.id}>{arrayElement.id}</td>
+							      <td>{arrayElement.name}</td>
+							      <td>{arrayElement.place}</td>
+							      <td>{arrayElement.date}</td>
+							      <td>{arrayElement.time}</td>
+							    </tr>
+						})}
+							  </tbody>
+							</table>
+    
+  </div>
+);
+
+
+
+ReactDOM.render(element, document.getElementById('root'));
+
