5253f822b8b583dfc53013648765fe4234ecc4b4
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-11-01 17:28:01 +0000

    wip

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import UserData from './UserData';
+
 
 class HomeComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      firstName: 'Janusz', 
-      sureName: 'Kovalski',
-      age: 57,
-      fullName: function() {
-        return (this.firstName + ' ' + this.sureName);
-      }
-    };
+      counter: 0
+    };          
   }
  
   render() {
     return (
       <div>
-        <UserData name={this.state.fullName()} age={this.state.age} />
+        <p>counter: {this.state.counter}</p>
+        <button onClick={() => this.setState({ counter: this.state.counter + 1 })}> increment </button>
       </div>
     );
   }
